e377f824b69e79d737f2069cc4188576a1017391
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-30 00:10:34 +0200

    Add files via upload

---
+++ Action Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1._Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> printStrings = strings => Console.WriteLine(string.Join(Environment.NewLine, strings));
+            printStrings(strings);
+        }
+    }
+}
