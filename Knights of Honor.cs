ab3186b62a0a4afcc56f26c7eb4b24b9d78b3bf4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-30 00:11:08 +0200

    Add files via upload

---
+++ Knights of Honor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _2._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string> print = item => Console.WriteLine($"Sir {item}");
+
+            foreach (string item in names)
+            {
+                print(item);
+            }
+        }
+    }
+}
