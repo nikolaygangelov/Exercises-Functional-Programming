2366fb970b90ac050779206df5657648df0fc141
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-30 00:11:23 +0200

    Add files via upload

---
+++ Custom Min Function.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            HashSet<int> numbers = new HashSet<int>(numbersInput);
+
+            
+            Func<HashSet<int>, int> minFunc = numbers =>
+            {
+                int minValue = int.MaxValue;
+                foreach (int number in numbers)
+                {
+                    if (number < minValue)
+                    {
+                        minValue = number;
+                    }
+                }
+                return minValue;
+            };
+            Console.WriteLine(minFunc(numbers));
+            
+        }
+    }
+}
